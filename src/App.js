0e0504ceac4d4e8a34d764af2cd6a7299082747a
inkxk_ <nokink@hotmail.com>
2022-03-02 14:58:55 +0700

    sidebar, featuredInfo completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./components/pages/home/Home";
+import "./app.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Topbar />
+            <div className="container">
+                <Sidebar />
+                <Home />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import "./featuredInfo.css";
+
+const FeaturedInfo = () => {
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <span className="featuredTitle">Revenue</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,415</span>
+                    <span className="featuredMoneyRate">
+                        -8.4
+                        <ArrowDownward className="featuredIcon negative" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared to last months</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Sales</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$4,678</span>
+                    <span className="featuredMoneyRate">
+                        -2.3
+                        <ArrowDownward className="featuredIcon negative" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared to last months</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Cost</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,225</span>
+                    <span className="featuredMoneyRate">
+                        +3.8
+                        <ArrowUpward className="featuredIcon" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared to last months</span>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedInfo;
